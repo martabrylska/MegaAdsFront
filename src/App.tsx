aac97cd2a8a39f6ee54c1117c734ac9c84a19462
Marta Brylska <tomica.marta@gmail.com>
2023-08-29 12:28:51 +0200

    added context for searching adds

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Header} from "./components/layouts/Header";
 import {Map} from "./components/Map/Map"
+import { SearchContext } from './contexts/search.context';
 
 export const App = () => {
-
+    const [search, setSearch] = useState('');
     return (
-      <>
+      <SearchContext.Provider value={{search, setSearch}}>
         <Header/>
         <Map/>
-      </>
+      </SearchContext.Provider>
   );
 }
--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import {MapContainer, Marker, Popup, TileLayer, useMap} from "react-leaflet";
+import React, {useContext, useEffect} from 'react';
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 
 import 'leaflet/dist/leaflet.css'
 import './Map.css'
 import '../../utils/fix-map-icon'
+import {SearchContext} from "../../contexts/search.context";
 
-export const Map = () => (
-    <div className="map">
-        <MapContainer center={[50.2657152,18.9945088]} zoom={20}>
+export const Map = () => {
+    const {search} = useContext(SearchContext);
+    useEffect(() => {
+        console.log(search);
+    }, [search]);
+
+    return <div className="map">
+        <h1>Search for {search}</h1>
+        <MapContainer center={[50.2657152, 18.9945088]} zoom={20}>
             <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
-            <Marker position={[50.2657152,18.9945088]}>
+            <Marker position={[50.2657152, 18.9945088]}>
                 <Popup>
                     <h2>It Focus</h2>
                     <p>Najlepsza firma</p>
                 </Popup>
             </Marker>
-            <Marker position={[50.2657152,18.9945088]}>
+            <Marker position={[50.2657152, 18.9945088]}>
                 <Popup>
                     <h2>It Focus</h2>
                     <p>Najlepsza firma</p>
@@ -26,4 +33,4 @@ export const Map = () => (
             </Marker>
         </MapContainer>
     </div>
-)
+}
