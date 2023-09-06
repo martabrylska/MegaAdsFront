431db08388e12381469e471fc1ae8583652972de
Marta Brylska <tomica.marta@gmail.com>
2023-09-06 12:02:11 +0200

    added geocoding and more urls to ad.

--- src/components/Map/SingleAd.tsx
+++ src/components/Map/SingleAd.tsx
@@ -7,6 +7,7 @@ interface Props {
 
 export const SingleAd = (props: Props) => {
     const [ad, setAd] = useState<AdEntity | null>(null);
+
     useEffect(() => {
         (async () => {
             const res = await fetch(`http://localhost:3001/ad/${props.id}`)
@@ -24,7 +25,9 @@ export const SingleAd = (props: Props) => {
         <p>{ad.description}</p>
         {!!ad.price && <p><b>{ad.price} zł</b></p>}
         <hr/>
-        <a href={ad.url} target="_blank">Otwórz ogłoszenie</a>
+        <p><a href={ad.url1} target="_blank">{ad.url1}</a></p>
+        {!!ad.url2 && <p><a href={ad.url2} target="_blank">{ad.url2}</a></p>}
+        {!!ad.url3 && <p><a href={ad.url3} target="_blank">{ad.url3}</a></p>}
     </>
 
 
